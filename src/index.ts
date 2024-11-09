14e026a72076771885ae0c88eef2df28474f4f32
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-09 13:22:06 +0200

    feat: Initialise express in application entry file

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import app from './app';
+import { getRequiredEnvVar } from './utils/getRequiredEnvVar';
+
+const port = getRequiredEnvVar('PORT');
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
