2813f83e91b9595c692f0683d435b95134c9d71d
ColourfulMelon <jordynnitch@gmail.com>
2024-11-16 16:34:53 +0200

    update task options

--- src/lib/zodSchemas.ts
+++ src/lib/zodSchemas.ts
@@ -1,21 +1,30 @@
 import { z } from 'zod';
 
+export enum models {
+	'SD-Base',
+	'Pony-XL',
+
+}
+
 export const zTask = z.object({
-    id: z.string().uuid(),
-    prompt: z.string(),
+	id: z.string().uuid(),
+	prompt: z.string(),
+	optimisePrompt: z.boolean().optional(),
+	faceDetailer: z.boolean().optional(),
+	modelOverride: z.nativeEnum(models).optional(),
 });
 
-export const zTaskCreate = zTask.pick({ prompt: true });
+export const zTaskCreate = zTask.omit({ id: true });
 
 export const zTaskStatus = z.object({
-    id: z.string().uuid(),
-    status: z.enum(['pending', 'completed']),
-    statusMessage: z.string(),
-    progress: z.number().int().min(0).max(1),
+	id: z.string().uuid(),
+	status: z.enum(['pending', 'completed']),
+	statusMessage: z.string(),
+	progress: z.number().int().min(0).max(1),
 });
 
 export const zTaskResult = z.object({
-    id: z.string().uuid(),
-    prompt: z.string(),
-    result: z.instanceof(Buffer),
+	id: z.string().uuid(),
+	prompt: z.string(),
+	result: z.instanceof(Buffer),
 });
