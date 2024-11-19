2b8d8a04df328f2c548a544517deb1200723038f
ColourfulMelon <jordynnitch@gmail.com>
2024-11-19 22:50:01 +0200

    update prompt for SD3.5

--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -59,19 +59,30 @@ export async function getTaskResult(id: string): Promise<Buffer> {
 	return await task.getResult();
 }
 
+const promptExamples = [
+	'Black and white coloring book page, simple rough 1960s cartoon black-and-white pen and ink line art, hand-drawn outline doodle of a smiling apple with a smiling worm sticking out of it on top of a stack of textbooks, overlay says Back to School! in a 1960s hand-written print.',
+	'Stylistic, 3D render of a cute tiny robot sitting with a puppy on a couch, surrounded by colorful sticky notes.',
+	'Expressionist painting of an astronaut walking on an alien planet with colorful balloons floating in the background. The scene is lit by soft lighting from the stars accentuating the presence of the astronaut in the vastness of space.',
+	'Watercolor painting of a nostalgic 1950s diner, with neon signs glowing outside and people enjoying milkshakes at the counter.',
+	'Simple illustration of a New York City office building in a modern comic book style. The colors should be rich and saturated with bright, energetic lighting and the background has interesting lines and textures.',
+	'Anime rendering of a quiet, rural train station during a rain shower, with reflections on the wet pavement. Umbrellas dot the scene as the rain creates ripples in nearby puddles, and a train waits in the background.',
+	'Wide view of a cute, isometric voxel cartoon 3D render of a vaporware apartment with a cat on a bed with starlight night view of the city from an open window in the background.',
+];
+
 export async function optimisePrompt(prompt: string): Promise<string> {
 	const chatCompletion = await llmClient.chat.completions.create({
 		messages: [
 			{ role: 'system', content: `You are an image generation prompting AI. Your job is to take a user prompt 
-			and convert it into the following format. Do not include any of the titles just rearrange the prompt to be 
-			in this format and add any required detail. The format is:
-			Describe the primary focus of the image, including the main subject and key actions or features.
-			Specify the desired artistic style or the medium in which the image should be rendered.
-			Describe the setting or background.
-			Convey the emotional tone or atmosphere.
-			Suggest dominant colors or overall color scheme.
-			End with relevant, comma separated tags for additional context or specifics not covered in the description.
-			Do not include the # character in your tags.
+			and convert it into the following format. Be concise and descriptive, do not include unnecessary words in your description.
+			The format is:
+			Style: Define the aesthetic direction, such as illustration style, painting medium, digital art style, or photography. Experiment and blend styles such as line art, watercolor, oil painting, surrealism, expressionism, and product photography
+			Subject and Action: If the image has a subject, the prompt should be written to amplify its presence first and any actions the subject takes afterward. Consider the images and prompts below.
+			Composition and Framing: Describe the desired composition and framing of the image by specifying close-up shots or wide-angle views.
+			Lighting and Color: Describe the lighting or shadows in the scene using terms like "backlight", "hard rim light", and "dynamic shadows"
+			Technical Parameters: Specify technical parameters using cinematic terms to guide the desired perspective and framing. Terms like “bird’s eye view,” “close-up,” “crane shot,” and “wide-angle shot” can help direct the composition effectively. Consider using terms like “fish-eye lens” for a curved look to achieve unique visual effects.
+			Text: The model can incorporate text into images. To achieve the best results, enclose the text in “double quotes” and keep the desired words or phrases short.
+			Examples:
+			${promptExamples.join('\n')}
 			` },
 			{ role: 'user', content: prompt },
 		],
