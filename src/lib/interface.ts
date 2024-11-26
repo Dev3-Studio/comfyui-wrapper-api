3e5bbdb2f3289d8e568fa449e8589df7383dd108
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-26 12:44:41 +0200

    refactor: Remove unused file

--- src/lib/interface.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-import { zTask, zTaskCreate, zTaskResult, zTaskStatus } from './zodSchemas';
-
-export type Task = z.infer<typeof zTask>;
-export type TaskCreate = z.infer<typeof zTaskCreate>;
-export type TaskStatus = z.infer<typeof zTaskStatus>;
-export type TaskResult = z.infer<typeof zTaskResult>;
