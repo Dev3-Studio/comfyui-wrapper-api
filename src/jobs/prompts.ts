a2c83159420035b39b6709eb4f7649ff8c781b00
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-26 22:59:31 +0200

    feat: Store prompts in database and results in R2

--- src/jobs/prompts.ts
+++ src/jobs/prompts.ts
@@ -1,6 +1,13 @@
 import { AnimeWorkflow, FantasyWorkflow, Layout, RealisticWorkflow, Workflow, Workflows } from '../core/workflows';
 import { getRandomSeed } from '../utils/getRandomSeed';
 import cron from 'node-cron';
+import { db } from '../db';
+import { promptsTable, resultsTable } from '../db/schema';
+import { z } from 'zod';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { getRequiredEnvVar } from '../utils/getRequiredEnvVar';
+import { eq } from 'drizzle-orm';
+import { getR2Client } from '../utils/getR2Client';
 
 const promptJobs = new Map<string, Workflow>();
 
@@ -35,6 +42,15 @@ export async function queuePromptJob(options: QueuePromptJobOptions) {
     void await workflow.startExecution();
     const promptId = workflow.promptId;
     if (!promptId) throw new Error('Error while queueing prompt job - promptId is undefined');
+    await db.insert(promptsTable).values({
+        id: promptId,
+        clientId,
+        text,
+        enhancedText,
+        layout,
+        workflow: options.workflow,
+        seed,
+    });
     promptJobs.set(promptId, workflow);
     return workflow;
 }
@@ -43,19 +59,74 @@ export function getPromptJob(jobId: string) {
     return promptJobs.get(jobId);
 }
 
-cron.schedule('*/30 * * * *', () => {
-    // Clear all prompt jobs that are completed
-    for (const [key, value] of promptJobs.entries()) {
-        if (value.progress?.value === 1) {
-            promptJobs.delete(key);
-        }
-    }
-});
-
-// Update database with statuses every 5 seconds
+/** Runs every 5 seconds
+ * - Inserts prompt job results into the database
+ * - Uploads completed jobs to S3 Bucket on Cloudflare R2
+ * - Deletes completed jobs from memory
+ */
 cron.schedule('*/5 * * * * *', () => {
     const entries = Array.from(promptJobs.entries());
     entries.forEach(async ([key, value]) => {
         const progress = value.progress;
+        
+        // Insert, on conflict update progress/result
+        const status = z.enum(['pending', 'completed']).parse(progress?.value === 1 ? 'completed' : 'pending');
+        const { status: statusMessage, value: progressValue, error } = progress ?? {};
+        
+        await db.insert(resultsTable).values({
+            promptId: key,
+            status,
+            statusMessage,
+            progress: progressValue,
+            error,
+        }).onConflictDoUpdate({
+            target: resultsTable.promptId,
+            set: {
+                status,
+                statusMessage,
+                progress: progressValue,
+                error,
+            },
+        });
+        
+        // Upload completed jobs to S3 Bucket
+        if (progressValue === 1) {
+            const r2Client = getR2Client();
+            const bucketName = getRequiredEnvVar('R2_BUCKET_NAME');
+            const objectKey = `results/${key}.png`;
+            const uploadParams = {
+                Bucket: bucketName,
+                Key: objectKey,
+                Body: await value.getResult(),
+                ContentType: 'image/png',
+            };
+            
+            try {
+                const command = new PutObjectCommand(uploadParams);
+                await r2Client.send(command);
+                // Update db with S3 URL
+                await db.update(resultsTable)
+                    .set({ s3Key: objectKey })
+                    .where(eq(resultsTable.promptId, key));
+            } catch (e) {
+                console.error(`Error while uploading prompt job ${key} to S3: ${e}`);
+                // Update db with error
+                await db.update(resultsTable)
+                    .set({ error: 'Could not upload completed image' })
+                    .where(eq(resultsTable.promptId, key));
+                
+                // Delete from memory
+                promptJobs.delete(key);
+            }
+        }
+        
+        // Clear completed jobs
+        if (progressValue === 1) {
+            promptJobs.delete(key);
+        }
+        if (error) {
+            console.error(`Error while processing prompt job ${key}: ${error}`);
+            promptJobs.delete(key);
+        }
     });
 });
--- src/services/prompts.ts
+++ src/services/prompts.ts
@@ -1,8 +1,15 @@
-import { getPromptJob, queuePromptJob } from '../jobs/prompts';
+import { queuePromptJob } from '../jobs/prompts';
 import { Prompt, PromptCreate, PromptStatus } from '../lib/zodSchemas';
 import { optimisePrompt } from '../core/llm';
 import { getRandomSeed } from '../utils/getRandomSeed';
 import { Layout, Workflows } from '../core/workflows';
+import { db } from '../db';
+import { resultsTable } from '../db/schema';
+import { eq } from 'drizzle-orm';
+import { getR2Client } from '../utils/getR2Client';
+import { getRequiredEnvVar } from '../utils/getRequiredEnvVar';
+import { GetObjectCommand } from '@aws-sdk/client-s3';
+import { Readable } from 'stream';
 
 export async function createPrompt(prompt: PromptCreate): Promise<Prompt> {
     const { text, clientId, workflowOverride, layoutOverride, seedOverride } = prompt;
@@ -33,32 +40,51 @@ export async function createPrompt(prompt: PromptCreate): Promise<Prompt> {
 }
 
 export async function getPromptStatus(promptId: string): Promise<PromptStatus> {
-    const prompt = getPromptJob(promptId);
-    if (!prompt) {
-        throw new Error('NOT_FOUND');
-    }
-    const progress = prompt.progress;
+    const data = await db
+        .select({
+            promptId: resultsTable.promptId,
+            status: resultsTable.status,
+            statusMessage: resultsTable.statusMessage,
+            progress: resultsTable.progress,
+        })
+        .from(resultsTable)
+        .limit(1)
+        .where(eq(resultsTable.promptId, promptId));
     
-    if (!prompt.promptId || !progress) {
-        throw new Error('NOT_FOUND');
-    }
-    return {
-        promptId,
-        progress: progress.value,
-        status: progress.value === 1 ? 'completed' : 'pending',
-        statusMessage: progress.status,
-    };
+    if (data.length === 0) throw new Error('NOT_FOUND');
+    return data[0];
 }
 
 export async function getPromptResult(promptId: string): Promise<Buffer> {
-    const prompt = getPromptJob(promptId);
-    if (!prompt) {
+    const [{ r2Key }] = await db
+        .select({
+            r2Key: resultsTable.s3Key,
+        })
+        .from(resultsTable)
+        .limit(1)
+        .where(eq(resultsTable.promptId, promptId));
+    if (!r2Key) throw new Error('NOT_FOUND');
+    
+    const r2Client = getR2Client();
+    const bucketName = getRequiredEnvVar('R2_BUCKET_NAME');
+    
+    try {
+        const command = new GetObjectCommand({
+            Bucket: bucketName,
+            Key: r2Key,
+        });
+        const response = await r2Client.send(command);
+        const streamToBuffer = async (stream: Readable) => {
+            const chunks = [];
+            for await (const chunk of stream) {
+                chunks.push(chunk);
+            }
+            return Buffer.concat(chunks);
+        };
+        
+        return await streamToBuffer(response.Body as Readable);
+    } catch (e) {
+        console.error(`Error while downloading prompt job ${promptId} from S3: ${e}`);
         throw new Error('NOT_FOUND');
     }
-    const progress = prompt.progress;
-    if (progress && progress.value !== 1) {
-        throw new Error('NOT_COMPLETED');
-    }
-    
-    return await prompt.getResult();
 }
