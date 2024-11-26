323f1ad93fce207f494220651015d71227947790
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-26 21:27:31 +0200

    feat: Add cleanup cron-job

--- src/jobs/prompts.ts
+++ src/jobs/prompts.ts
@@ -1,5 +1,6 @@
 import { AnimeWorkflow, FantasyWorkflow, Layout, RealisticWorkflow, Workflow, Workflows } from '../core/workflows';
 import { getRandomSeed } from '../utils/getRandomSeed';
+import cron from 'node-cron';
 
 const promptJobs = new Map<string, Workflow>();
 
@@ -41,3 +42,20 @@ export async function queuePromptJob(options: QueuePromptJobOptions) {
 export function getPromptJob(jobId: string) {
     return promptJobs.get(jobId);
 }
+
+cron.schedule('*/30 * * * *', () => {
+    // Clear all prompt jobs that are completed
+    for (const [key, value] of promptJobs.entries()) {
+        if (value.progress?.value === 1) {
+            promptJobs.delete(key);
+        }
+    }
+});
+
+// Update database with statuses every 5 seconds
+cron.schedule('*/5 * * * * *', () => {
+    const entries = Array.from(promptJobs.entries());
+    entries.forEach(async ([key, value]) => {
+        const progress = value.progress;
+    });
+});
