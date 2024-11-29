0f8f1521924e5312c016647b3bd5f1948f44a4a8
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-29 12:29:04 +0200

    fix: Update getOutput function to use filename instead of promptId

--- src/services/outputs.ts
+++ src/services/outputs.ts
@@ -1,28 +1,16 @@
-import { resultsTable } from '../db/schema';
-import { eq } from 'drizzle-orm';
-import { db } from '../db';
 import { getRequiredEnvVar } from '../utils/getRequiredEnvVar';
 import { getR2Client } from '../utils/getR2Client';
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { Readable } from 'stream';
 
-export async function getOutput(promptId: string): Promise<Buffer> {
-    const [{ r2Key }] = await db
-        .select({
-            r2Key: resultsTable.s3Key,
-        })
-        .from(resultsTable)
-        .limit(1)
-        .where(eq(resultsTable.promptId, promptId));
-    if (!r2Key) throw new Error('NOT_FOUND');
-    
+export async function getOutput(filename: string): Promise<Buffer> {
     const r2Client = getR2Client();
     const bucketName = getRequiredEnvVar('R2_BUCKET_NAME');
     
     try {
         const command = new GetObjectCommand({
             Bucket: bucketName,
-            Key: r2Key,
+            Key: filename,
         });
         const response = await r2Client.send(command);
         const streamToBuffer = async (stream: Readable) => {
@@ -32,10 +20,8 @@ export async function getOutput(promptId: string): Promise<Buffer> {
             }
             return Buffer.concat(chunks);
         };
-        
         return await streamToBuffer(response.Body as Readable);
     } catch (e) {
-        console.error(`Error while downloading prompt job ${promptId} from S3: ${e}`);
         throw new Error('NOT_FOUND');
     }
 }
