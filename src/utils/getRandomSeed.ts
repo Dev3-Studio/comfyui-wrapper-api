e50bdfd39427473c420925ca58b9cecf60c634c0
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-30 22:45:15 +0200

    chore: Change bit precision for random seeds

--- src/utils/getRandomSeed.ts
+++ src/utils/getRandomSeed.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto';
 
 export function getRandomSeed() {
-    return parseInt(crypto.randomBytes(8).toString('hex'), 16);
+    return parseInt(crypto.randomBytes(4).toString('hex'), 16);
 }
