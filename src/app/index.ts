63a4a9cccae3701d1c001f72f5dad168f9db82f5
Xirynx <64374129+Xirynx@users.noreply.github.com>
2024-11-30 12:02:01 +0200

    feat: Add cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
 import prompts from '../routes/prompts';
 import outputs from '../routes/outputs';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
